636969a173b17996598b7ee5a33e196fce9c5886
Holy Obina <obinakelle@gmail.com>
2025-04-30 22:06:31 +0800

    Fix ALLOWED_HOSTS for production

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -16,9 +16,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 import os
 
-ALLOWED_HOSTS = [
-    os.getenv("RENDER_EXTERNAL_HOSTNAME", "localhost")
-]
+ALLOWED_HOSTS = ['ai-recommender-ecommerce-190h.onrender.com', 'localhost', '127.0.0.1']
 
 
 
